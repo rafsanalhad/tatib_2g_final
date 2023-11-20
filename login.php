641607732514ba57bab429771208e0cf4f8a6590
Irshandy Aditya <irshandy.aditya@gmail.com>
2023-11-20 11:27:25 +0700

    Membenarkan login.php dan loginProses.php
    menambahkan name dan mengubah button menjadi input dengan tipe submit pada login.php dan membenarkan loginProses agar dapat memproses password dengan md5

--- login.php
+++ login.php
@@ -9,14 +9,14 @@
 <body>
     <div class="flex flex-col justify-center items-center h-screen">
         <img src="assets/login/profil.png" alt="profil" class="scale-75">
-        <form action="index.php" method="post">
+        <form action="loginProses.php" method="post">
             <div class="max-w-screen-sm bg-white rounded-[62px] border-4 border-sky-600 flex justify-center items-center">
                 <div class="flex flex-col p-10">
                     <label for="username" class="text-neutral-400 text-2xl font-normal font-['Inter'] m-auto">Username:</label>
-                    <input type="text" id="username" class="w-[250px] h-[40px] border border-neutral-400 rounded-[10px]"/>
+                    <input type="text" id="username" name="username" class="w-[250px] h-[40px] border border-neutral-400 rounded-[10px] pl-1"/>
                     <label for="password" class="text-neutral-400 text-2xl font-normal font-['Inter'] m-auto mt-[10px]">Password:</label>
-                    <input type="password" id="password" class="w-[250px] h-[40px] border border-neutral-400 rounded-[10px]"/>
-                    <button class="w-[180px] h-[50px] items-center bg-sky-600 rounded-[31px] text-white text-2xl font-normal font-['Inter'] m-auto mt-5 act:bg-sky-600">Login</button>
+                    <input type="password" id="password" name="password" class="w-[250px] h-[40px] border border-neutral-400 rounded-[10px] pl-1"/>
+                    <input type="submit" class="w-[180px] h-[50px] items-center bg-sky-600 rounded-[31px] text-white text-2xl font-normal font-['Inter'] m-auto mt-5 act:bg-sky-600" value="Login" />
                     <a href="signUp.php" class="m-auto text-lg text-gray-600">create account</a>
                 </div>
             </div>
