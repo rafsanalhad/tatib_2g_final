c4811b6bff019e34803c4e1cea35aaf19c4b3085
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2023-12-19 08:16:00 +0700

    join table

--- app/models/Kompen_model.php
+++ app/models/Kompen_model.php
@@ -8,6 +8,7 @@ class Kompen_model
     private $table4 = 'pelanggaran';
     private $table5 = 'prodi';
     private $table6 = 'riwayat';
+    private $table7 = 'sanksi_pelanggaran';
     private $db;
 
 
@@ -20,7 +21,7 @@ class Kompen_model
     {
         $this->db->query('SELECT * FROM ' . $this->table6 . ' a INNER JOIN ' . $this->table2 . ' b ON a.nim = b.nim INNER JOIN 
                         ' . $this->table3 . ' c ON a.pengaduan_id = c.pengaduan_id INNER JOIN ' . $this->table4 . ' d 
-                        ON c.pelanggaran_id = d.pelanggaran_id WHERE a.nim = :nim');
+                        ON c.pelanggaran_id = d.pelanggaran_id INNER JOIN ' . $this->table7 . ' e ON d.sanksi_id = e.sanksi_id WHERE a.nim = :nim');
         $this->db->bind('nim', $nim);
         return $this->db->resultSet();
     }
